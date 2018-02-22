78a59a487d8ee217970483afe4925b949906077c
amran0917 <bsse0917@iit.du.ac.bd>
2018-02-22 08:44:45 +0600

    Test

---
+++ spl1-source.java
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namspace std;
+
+int main()
+{
+
+/* amrat
+dsr sl
+askd f
+a dlfak;*/
+//
+// heloo
+//java
+
+int a,b,c;
+float d,e;
+string s;
+char ch;
+
+	cout<<"Assalamu alaikum" << endl;
+	return 0;
+
+
+
+}
