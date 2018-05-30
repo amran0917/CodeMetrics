ee27f5d8b02d274e909c8be1f2e0eb870c65c70d
Amran Hossain <35799076+amran0917@users.noreply.github.com>
2018-05-30 09:00:09 +0600

    Add files via upload

---
+++ SPL.java
@@ -0,0 +1,36 @@
+Public class SPL{
+   
+    
+        Scanner input = new Scanner(System.in);
+        SPL spl;
+        spl = new SPL();
+        int a,b;
+        float f;
+        a = input.nextInt();
+        
+        f = input.nextDouble();
+              
+            spl.getAddResult();
+            
+            spl.getMult();
+              
+        public void getName ()
+        {
+        ////get name
+        /*
+        ////return nothing */
+        
+        }
+          
+        public void getdivide(int a,int v)
+        {
+			
+            int res = v/a;
+        }
+         
+        double c = getdivide();
+        
+        system.out.println();
+        
+    
+}
